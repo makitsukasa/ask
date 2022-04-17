a3f25c21b6dcbd2a20f0f6c2d3f70206b68056a8
makitsukasa <allium.fistulosum.39@gmail.com>
2022-04-18 01:08:45 +0900

    first commit

---
+++ $src/main.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+	tbody = document.getElementById("tbody");
+	for (var anken of ankens) {
+		// <tr>
+		var tr = document.createElement("tr");
+		tbody.append(tr);
+			// <th>
+			var th = document.createElement("th");
+			tr.append(th);
+			th.innerHTML = anken;
+			// </th>
+			// <td>
+			var td = document.createElement("td");
+			tr.append(td);
+				// <iframe>
+				var iframe = document.createElement("iframe");
+				td.append(iframe);
+				iframe.src = "./" + anken + "/$status.html";
+				// </iframe>
+			// </td>
+			// <td>
+			var td = document.createElement("td");
+			tr.append(td);
+				// <a>
+				var a = document.createElement("a");
+				td.append(a);
+				a.href = "./" + anken + "/$status.html";
+				a.target = "_blank";
+				a.innerHTML = "status";
+				// </a>
+			// </td>
+			// <td>
+			var td = document.createElement("td");
+			tr.append(td);
+				// <a>
+				var a = document.createElement("a");
+				td.append(a);
+				a.href = "./" + anken + "/$before.html";
+				a.target = "_blank";
+				a.innerHTML = "before";
+				// </a>
+			// </td>
+			// <td>
+			var td = document.createElement("td");
+			tr.append(td);
+				// <a>
+				var a = document.createElement("a");
+				td.append(a);
+				a.href = "./" + anken + "/$after.html";
+				a.target = "_blank";
+				a.innerHTML = "after";
+				// </a>
+			// </td>
+		// </tr>
+	}
+};
---
+++ fetch.bat
@@ -0,0 +1,7 @@
+@echo off
+@rem anken名(="???? *"に一致するフォルダ名)の一覧をファイルに書き込む
+@set filename=$src\ankens.js
+@echo const ankens = [> %filename%
+@for /d %%a in ("???? *") do echo     "%%a",>> %filename%
+@echo ]>> %filename%
+@echo on
